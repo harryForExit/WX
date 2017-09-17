4a7ba69b83830d7bc31c5f00f103a461fd3e8ae9
Administrator <616535820@qq.com>
2017-09-18 07:50:42 +0800

    Revert "11xm"
    This reverts commit 84a04a59b4b69606a3fd76c1e8fb4305a0f333ca.
    
    # Conflicts:
    #	pom.xml

---
+++ src/main/java/util/OpenIdUtils.java
@@ -0,0 +1,45 @@
+package util;
+
+import com.alibaba.fastjson.JSON;
+import entity.menu.MenuManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+public class OpenIdUtils {
+    private static Logger log = LoggerFactory.getLogger(OpenIdUtils.class);
+
+    // 第三方用户唯一凭证
+    private static String appId = "wxba50d95321107e8f";
+    // 第三方用户唯一凭证密钥
+    private static String appSecret = "ea9ae1c3cae3086f6e6eb2e427fb519b";
+
+    private static String url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=APPID&secret=SECRET&code=CODE&grant_type=authorization_code";
+
+    public static void requireOpenId(HttpServletRequest request){
+        HttpClientUtil httpClientUtil = new HttpClientUtil();
+
+        String str = httpClientUtil.doGet(getUrl(request.getParameter("code")),null);
+
+        log.info("获取openid返回"+str);
+
+        Map map = JSON.parseObject(str, Map.class);
+
+        log.info("openid="+map.get("openid"));
+
+        request.getSession().setAttribute("openid",map.get("openid"));
+
+        request.getSession().setAttribute("access_token",map.get("access_token"));
+    }
+
+
+    private static String getUrl(String code){
+        String herfUrl = url.replace("APPID",appId).replace("SECRET",appSecret).replace("CODE",code);
+
+        log.info("url为"+herfUrl);
+        return herfUrl;
+    }
+
+}
---
+++ src/main/java/util/PayUtil.java
@@ -0,0 +1,16 @@
+package util;
+
+import java.util.Random;
+
+public class PayUtil {
+
+    public static String create_nonce_str() {
+        String chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        String res = "";
+        for (int i = 0; i < 16; i++) {
+            Random rd = new Random();
+            res += chars.charAt(rd.nextInt(chars.length() - 1));
+        }
+        return res;
+    }
+}
